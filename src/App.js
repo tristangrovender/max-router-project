faed1fec5a0c5a9e86b08eb44764cac460b947a7
Tristan Grovender <46428421+tristangrovender@users.noreply.github.com>
2021-12-08 11:33:05 -0600

    very basic routing

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
+import Products from "./pages/Products";
+import { Route } from "react-router-dom";
+import Welcome from "./pages/Welcome";
+
 function App() {
   return (
     <div>
-      <h2>Let's get started!</h2>
+      <Route path="/welcome">
+        <Welcome />
+      </Route>
+      <Route path="/products">
+        <Products />
+      </Route>
     </div>
   );
 }
